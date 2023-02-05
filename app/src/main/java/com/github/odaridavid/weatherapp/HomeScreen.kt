c472d237bf8b75df1dc9193e6de3a33c175d847a
odaridavid <davidkibzodari@gmail.com>
2023-02-05 23:50:40 +0100

    Connect viewmodel to screen

---
+++ app/src/main/java/com/github/odaridavid/weatherapp/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.github.odaridavid.weatherapp
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(state: HomeScreenViewState) {
+  Log.i("State","$state")
+}
--- app/src/main/java/com/github/odaridavid/weatherapp/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/MainActivity.kt
@@ -3,43 +3,33 @@ package com.github.odaridavid.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.odaridavid.weatherapp.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: HomeScreenViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val state =
+                        viewModel.state.collectAsState(initial = HomeScreenViewState()).value
+                    HomeScreen(state = state)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
-    }
-}
