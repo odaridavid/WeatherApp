c28814a08ef0999512a9ef404fa50b690c648a04
odaridavid <davidkibzodari@gmail.com>
2023-02-05 23:45:17 +0100

    Change and define types for service

--- app/src/main/java/com/github/odaridavid/weatherapp/OpenWeatherService.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/OpenWeatherService.kt
@@ -1,18 +1,19 @@
 package com.github.odaridavid.weatherapp
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface OpenWeatherService {
 
     @GET("/data/3.0/onecall")
-    fun getWeatherData(
-        @Query("lat") latitude: Float,
-        @Query("lon") longitude: Float,
+    suspend fun getWeatherData(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
         @Query("appid") appid: String,
         @Query("exclude") excludedInfo: String,
         @Query("units") units: String,
         @Query("lang") language: String
-    )
-    // TODO Define response structure as data class
+    ): Response<WeatherResponse>
+
 }
