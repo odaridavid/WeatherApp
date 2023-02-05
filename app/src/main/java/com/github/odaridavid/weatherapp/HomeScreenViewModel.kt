e83dd9a57022dcdc1d3bcc83d343e29b49bde5a7
odaridavid <davidkibzodari@gmail.com>
2023-02-05 23:50:16 +0100

    Add viewmodel class

---
+++ app/src/main/java/com/github/odaridavid/weatherapp/HomeScreenViewModel.kt
@@ -0,0 +1,64 @@
+package com.github.odaridavid.weatherapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeScreenViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(HomeScreenViewState(isLoading = true))
+    val state: StateFlow<HomeScreenViewState> = _state
+
+    init {
+        viewModelScope.launch {
+            weatherRepository.fetchWeatherData().collect { result ->
+                processResult(result)
+            }
+        }
+    }
+
+    private fun processResult(result: Result<Weather>) {
+        when {
+            result.isSuccess -> {
+                val weatherData = result.getOrThrow()
+                setState {
+                    copy(
+                        weather = weatherData,
+                        isLoading = false,
+                        error = null
+                    )
+                }
+            }
+            result.isFailure -> {
+                // TODO Localise errors
+                setState {
+                    copy(
+                        isLoading = false,
+                        error = result.exceptionOrNull()
+                            ?: Throwable("Unknown error occurred")
+                    )
+                }
+            }
+        }
+    }
+
+    private fun setState(stateReducer: HomeScreenViewState.() -> HomeScreenViewState) {
+        viewModelScope.launch {
+            _state.emit(stateReducer(state.value))
+        }
+    }
+
+}
+
+data class HomeScreenViewState(
+    val weather: Weather? = null,
+    val isLoading: Boolean = false,
+    val error: Throwable? = null
+)
