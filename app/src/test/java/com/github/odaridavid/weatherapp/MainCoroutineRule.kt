1a791eed31a27ecddd9e2963ec52d23ca1b210de
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-04-21 18:55:49 +0300

    Main Coroutine Cleanup

--- app/src/test/java/com/github/odaridavid/weatherapp/MainCoroutineRule.kt
+++ app/src/test/java/com/github/odaridavid/weatherapp/MainCoroutineRule.kt
@@ -2,26 +2,20 @@ package com.github.odaridavid.weatherapp
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
-    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+
     override fun starting(description: Description) {
-        super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description) {
-        super.finished(description)
         Dispatchers.resetMain()
-        dispatcher.cancel()
     }
 }
