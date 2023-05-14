1039d3230cc92df61f0f26c56fc562bc3a6b73e9
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-05-14 13:40:14 +0300

    DI for Local module and updated Manifest file to include notification permission

---
+++ app/src/main/java/com/github/odaridavid/weatherapp/di/LocalModule.kt
@@ -0,0 +1,37 @@
+package com.github.odaridavid.weatherapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.github.odaridavid.weatherapp.data.weather.local.WeatherDatabase
+import com.github.odaridavid.weatherapp.data.weather.local.dao.WeatherDao
+import com.github.odaridavid.weatherapp.util.NotificationUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+    @Provides
+    @Singleton
+    fun provideWeatherDatabase(@ApplicationContext app: Context): WeatherDatabase {
+        return Room
+            .databaseBuilder(app, WeatherDatabase::class.java, "weather_database")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideNotificationUtil(@ApplicationContext context: Context): NotificationUtil {
+        return NotificationUtil(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWeatherDao(db: WeatherDatabase): WeatherDao {
+        return db.weatherDao()
+    }
+}
