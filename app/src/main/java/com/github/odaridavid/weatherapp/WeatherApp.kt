6f50a418a2ba050faafaae94e773a70ff0d5061d
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-05-14 13:45:46 +0300

    updated WeatherApp to getWorkManagerConfigs and included Timber for Debugging

--- app/src/main/java/com/github/odaridavid/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/WeatherApp.kt
@@ -1,7 +1,22 @@
 package com.github.odaridavid.weatherapp
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class WeatherApp : Application()
+class WeatherApp : Application(),  Configuration.Provider{
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder().setWorkerFactory(workerFactory).build()
+}
