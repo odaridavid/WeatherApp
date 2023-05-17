545579a05f2ca07f4b5aa0fa9c39538735acf859
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-05-17 09:16:51 +0300

    Updated Fakes with the corrected mapped responses

--- app/src/test/java/com/github/odaridavid/weatherapp/Fakes.kt
+++ app/src/test/java/com/github/odaridavid/weatherapp/Fakes.kt
@@ -2,25 +2,51 @@ package com.github.odaridavid.weatherapp
 
 import com.github.odaridavid.weatherapp.core.model.CurrentWeather
 import com.github.odaridavid.weatherapp.core.model.Weather
+import com.github.odaridavid.weatherapp.core.model.WeatherInfo
 import com.github.odaridavid.weatherapp.data.weather.CurrentWeatherResponse
+import com.github.odaridavid.weatherapp.data.weather.WeatherInfoResponse
 import com.github.odaridavid.weatherapp.data.weather.WeatherResponse
+import com.github.odaridavid.weatherapp.data.weather.local.entity.WeatherEntity
 
 val fakeSuccessWeatherResponse = WeatherResponse(
     current = CurrentWeatherResponse(
         temperature = 3.0f,
         feelsLike = 2.0f,
-        weather = listOf()
+        weather = listOf(WeatherInfoResponse(
+            id = 1,
+            main = "fake",
+            description = "fake",
+            icon = "fake"
+        ))
     ),
-    hourly = listOf(),
-    daily = listOf()
+    hourly = emptyList(),
+    daily = emptyList()
 )
 
 val fakeSuccessMappedWeatherResponse = Weather(
     current = CurrentWeather(
         temperature = "3°C",
         feelsLike = "2°C",
-        weather = listOf()
+        weather = listOf(WeatherInfo(
+            id = 0,
+            main = "fake",
+            description = "fake",
+            icon = "http://openweathermap.org/img/wn/fake@2x.png"
+        ))
     ),
-    hourly = listOf(),
-    daily = listOf()
+    hourly = emptyList(),
+    daily = emptyList()
+)
+val fakeSuccessMappedEntityResponse = WeatherEntity(
+    hourly = emptyList(),
+    daily = emptyList(),
+    dt =  10L,
+    id = 1,
+    feels_like = 2.0f,
+    temp = 3.0f,
+    temp_max = 0.0f,
+    temp_min = 0.0f,
+    description = "fake",
+    icon = "fake",
+    main = "fake",
 )
