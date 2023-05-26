078cfebee60a3618a91c5f80ed0d22b69e07f6c0
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-05-26 10:33:25 +0300

    removed timber config

--- app/src/main/java/com/github/odaridavid/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/WeatherApp.kt
@@ -3,20 +3,27 @@ package com.github.odaridavid.weatherapp
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.github.odaridavid.weatherapp.worker.WeatherUpdateScheduler
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
 class WeatherApp : Application(),  Configuration.Provider{
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var weatherUpdateScheduler: WeatherUpdateScheduler
+
+    override fun onCreate() {
+        super.onCreate()
+        schedulePeriodicWeatherUpdates()
+    }
+
     override fun getWorkManagerConfiguration(): Configuration =
         Configuration.Builder().setWorkerFactory(workerFactory).build()
+
+    private fun schedulePeriodicWeatherUpdates() {
+        weatherUpdateScheduler.schedulePeriodicWeatherUpdates()
+    }
 }
