09a8f5930cb128bfb69328659ccbddf95f305e0f
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-06-19 08:30:00 +0300

    refactored weather update scheduler

--- app/src/main/java/com/github/odaridavid/weatherapp/worker/WeatherUpdateScheduler.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/worker/WeatherUpdateScheduler.kt
@@ -1,6 +1,5 @@
 package com.github.odaridavid.weatherapp.worker
 
-import android.content.Context
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
@@ -9,7 +8,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class WeatherUpdateScheduler @Inject constructor(
-    private val context: Context
+    private val workManager: WorkManager
 ) {
     private val constraints = Constraints.Builder()
         .setRequiredNetworkType(NetworkType.CONNECTED)
@@ -22,6 +21,6 @@ class WeatherUpdateScheduler @Inject constructor(
             .setConstraints(constraints)
             .build()
 
-        WorkManager.getInstance(context).enqueue(refreshWeatherRequest)
+        workManager.enqueue(refreshWeatherRequest)
     }
 }
