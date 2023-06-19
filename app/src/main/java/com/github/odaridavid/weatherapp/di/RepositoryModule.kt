ae05432d79d658806747cae5b08132f7b2e876c7
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-06-19 08:27:45 +0300

    updated viewmodel component with singleton component

--- app/src/main/java/com/github/odaridavid/weatherapp/di/RepositoryModule.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/di/RepositoryModule.kt
@@ -12,9 +12,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 interface RepositoryModule {
 
     @Binds
