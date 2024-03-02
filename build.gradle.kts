7e4948e3ba183e06d43c7b84f889f2cc1cd73373
odaridavid <davidkibzodari@gmail.com>
2024-03-02 14:32:50 +0100

    add versioning dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,15 @@ plugins {
     alias(libs.plugins.mapsplatform.secrets.gradle.plugin) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.nl.littlerobots.versioning) apply false
+    alias(libs.plugins.com.github.ben.manes.gradle.versions.plugin) apply false
 }
 
 buildscript {
     dependencies {
         classpath(libs.com.google.services)
         classpath(libs.com.firebase.crashlytics.plugin)
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.51.0")
+        classpath("nl.littlerobots.vcu:plugin:0.8.4")
     }
 }
