5c5bbf193fdae378f2f1065c496d16b76e048dca
odaridavid <davidkibzodari@gmail.com>
2024-03-07 01:15:41 +0100

    Add ios app to the cocktail

---
+++ iOSApp/iOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp
+//
+//  Created by David Odari Kiribwa on 07.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
