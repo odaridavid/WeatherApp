b7ef10235dc73fa310cc84b210c99f57867f5ba3
odaridavid <davidkibzodari@gmail.com>
2024-03-07 01:16:03 +0100

    remove unused error

--- app/src/main/java/com/github/odaridavid/weatherapp/ui/home/Mappers.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/ui/home/Mappers.kt
@@ -9,6 +9,5 @@ fun ErrorType.toResourceId(): Int = when (this) {
     ErrorType.SERVER -> R.string.error_server
     ErrorType.GENERIC -> R.string.error_generic
     ErrorType.IO_CONNECTION -> R.string.error_connection
-    ErrorType.UNAUTHORIZED -> R.string.error_unauthorized
     ErrorType.CLIENT -> R.string.error_client
 }
