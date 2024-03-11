0037c5417f53d8461b438133e63f9e7e45e8ba4b
odaridavid <davidkibzodari@gmail.com>
2024-03-11 06:22:50 +0100

    replace components with new ones

--- app/src/main/java/com/github/odaridavid/weatherapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/github/odaridavid/weatherapp/ui/home/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.github.odaridavid.weatherapp.ui.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,13 +22,13 @@ import com.github.odaridavid.weatherapp.core.model.CurrentWeather
 import com.github.odaridavid.weatherapp.core.model.DailyWeather
 import com.github.odaridavid.weatherapp.core.model.HourlyWeather
 import com.github.odaridavid.weatherapp.designsystem.WeatherAppTheme
-import com.github.odaridavid.weatherapp.designsystem.molecule.ErrorTextWithAction
-import com.github.odaridavid.weatherapp.designsystem.molecule.ForecastedTime
-import com.github.odaridavid.weatherapp.designsystem.molecule.Subtitle
-import com.github.odaridavid.weatherapp.designsystem.molecule.Temperature
-import com.github.odaridavid.weatherapp.designsystem.molecule.TemperatureHeadline
-import com.github.odaridavid.weatherapp.designsystem.molecule.WeatherIcon
+import com.github.odaridavid.weatherapp.designsystem.molecule.LargeLabel
+import com.github.odaridavid.weatherapp.designsystem.molecule.RemoteImage
+import com.github.odaridavid.weatherapp.designsystem.organism.ForecastedTime
 import com.github.odaridavid.weatherapp.designsystem.organism.HomeTopBar
+import com.github.odaridavid.weatherapp.designsystem.organism.Temperature
+import com.github.odaridavid.weatherapp.designsystem.organism.TemperatureHeadline
+import com.github.odaridavid.weatherapp.designsystem.templates.ErrorScreen
 import com.github.odaridavid.weatherapp.designsystem.templates.LoadingScreen
 
 @Composable
@@ -72,30 +71,28 @@ fun HomeScreen(
 }
 
 @Composable
-private fun ColumnScope.ErrorScreen(errorMsgId: Int, onTryAgainClicked: () -> Unit) {
-    Spacer(modifier = Modifier.weight(WeatherAppTheme.weight.HALF))
-    ErrorTextWithAction(
-        errorMessageId = errorMsgId,
-        modifier = Modifier.padding(WeatherAppTheme.dimens.medium)
-    ) {
-        onTryAgainClicked()
-    }
-    Spacer(modifier = Modifier.Companion.weight(WeatherAppTheme.weight.HALF))
-}
-
-@Composable
 private fun CurrentWeatherWidget(currentWeather: CurrentWeather) {
     Column {
-        Subtitle(text = stringResource(id = R.string.home_title_currently))
+        LargeLabel(
+            text = stringResource(id = R.string.home_title_currently),
+            modifier = Modifier.padding(
+                horizontal = WeatherAppTheme.dimens.medium,
+                vertical = WeatherAppTheme.dimens.small
+            )
+        )
 
         TemperatureHeadline(temperature = currentWeather.temperature)
 
-        Subtitle(
+        LargeLabel(
             text = stringResource(
                 id = R.string.home_feels_like_description,
                 currentWeather.feelsLike
             ),
-            color = MaterialTheme.colorScheme.secondary
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(
+                horizontal = WeatherAppTheme.dimens.medium,
+                vertical = WeatherAppTheme.dimens.small
+            )
         )
     }
 }
@@ -103,7 +100,13 @@ private fun CurrentWeatherWidget(currentWeather: CurrentWeather) {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun HourlyWeatherWidget(hourlyWeatherList: List<HourlyWeather>) {
-    Subtitle(text = stringResource(id = R.string.home_today_forecast_title))
+    LargeLabel(
+        text = stringResource(id = R.string.home_today_forecast_title),
+        modifier = Modifier.padding(
+            horizontal = WeatherAppTheme.dimens.medium,
+            vertical = WeatherAppTheme.dimens.small
+        )
+    )
 
     LazyRow(modifier = Modifier.padding(WeatherAppTheme.dimens.medium)) {
         items(hourlyWeatherList) { hourlyWeather ->
@@ -118,8 +121,8 @@ private fun HourlyWeatherWidget(hourlyWeatherList: List<HourlyWeather>) {
 @Composable
 private fun HourlyWeatherRow(hourlyWeather: HourlyWeather, modifier: Modifier) {
     Row(modifier = modifier) {
-        WeatherIcon(
-            iconUrl = hourlyWeather.weather.first().icon,
+        RemoteImage(
+            url = hourlyWeather.weather.first().icon,
             contentDescription = hourlyWeather.weather.first().description,
             modifier = Modifier
                 .padding(WeatherAppTheme.dimens.extraSmall)
@@ -139,7 +142,13 @@ private fun HourlyWeatherRow(hourlyWeather: HourlyWeather, modifier: Modifier) {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun DailyWeatherWidget(dailyWeatherList: List<DailyWeather>) {
-    Subtitle(text = stringResource(id = R.string.home_weekly_forecast_title))
+    LargeLabel(
+        text = stringResource(id = R.string.home_weekly_forecast_title),
+        modifier = Modifier.padding(
+            horizontal = WeatherAppTheme.dimens.medium,
+            vertical = WeatherAppTheme.dimens.small
+        )
+    )
 
     LazyColumn(modifier = Modifier.padding(WeatherAppTheme.dimens.medium)) {
         items(dailyWeatherList) { dailyWeather ->
@@ -155,8 +164,8 @@ private fun DailyWeatherRow(dailyWeather: DailyWeather, modifier: Modifier) {
             .padding(WeatherAppTheme.dimens.small)
             .fillMaxWidth()
     ) {
-        WeatherIcon(
-            iconUrl = dailyWeather.weather.first().icon,
+        RemoteImage(
+            url = dailyWeather.weather.first().icon,
             contentDescription = dailyWeather.weather.first().description,
             modifier = Modifier
                 .padding(WeatherAppTheme.dimens.extraSmall)
